77ca12e72bbcb31d719a59a430d6344fe66febe4
Aryan Jadhav <aryanjadhav54321@gmail.com>
2026-05-17 16:46:00 +0530

    Folder Structure Updated

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,7 @@ from src.utils import read_csv_data
 
 @dataclass
 class DataIngestionConfig():
+    raw_data_path: str = os.path.join("artifacts", "raw_data.csv")
     train_data_path: str = os.path.join("artifacts","train_data.csv")
     test_data_path: str = os.path.join("artifacts","test_data.csv")
     
@@ -23,13 +24,16 @@ class DataIngestion():
             df = read_csv_data()
             logging.info("Data Reading Complete")
             
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+            logging.info("raw data added to artifacts")
+            
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            logging.info("train_test_split complete")
             
             train_set.to_csv(self.ingestion_config.train_data_path, index=False)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False)
+            logging.info("train_set and test_set added in artifacts")
             
-            logging.info("train_test_split complete")
             
         except Exception as e:
             logging.info("Error Occured")
--- src/utils.py
+++ src/utils.py
@@ -9,9 +9,10 @@ from src.exception import CustomException
 
 def read_csv_data():
     try:
-        project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-        file_path = os.path.join(project_root, "data", "processed", "cleaned_atm_transactions.csv")
-        df = pd.read_csv(file_path)
+        file_path = os.path.dirname(__file__)
+        csv_path = os.path.join(file_path, "notebooks", "data", "processed" , "cleaned_atm_transactions.csv")
+        
+        df = pd.read_csv(csv_path)
         return df
     except Exception as e:
         logging.info("Error Occured")
